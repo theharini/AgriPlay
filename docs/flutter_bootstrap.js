4d466c7a76bba1d6c7a0b1315a6e0ce269795e53
Harini.R <harinirajarv@gmail.com>
2025-09-23 03:23:06 +0530

    Update Flutter web build for GitHub Pages

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2827097671"
+    serviceWorkerVersion: "255837105"
   }
 });
